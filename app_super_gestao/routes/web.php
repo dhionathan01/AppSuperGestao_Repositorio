0353ed1b42d275b53bfe20527082d5f89d3787db
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-18 00:13:08 -0300

    feat: :sparkles: (Rotas) Implementando
    Implementando as rotas sobre-nos e contato

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Iniciando Curso LaraveL";
+});
+
+Route::get('/sobre-nos', function () {
+    return "Sobre Nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
 });
