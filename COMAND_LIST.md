27581b81807fc14aca56f5071da822b7af6888c3
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-18 08:22:43 -0300

    feat: :sparkles: Criando controller e definindo uma action
    Criando o controllador PrincipalController definindo um action para a rota principal

---
+++ COMAND_LIST.md
@@ -0,0 +1,6 @@
+## Arquivo para salvar os comandos executados nas aulas:
+
+* Criando controllers principal, sobrenos e contato:
+```
+php artisan make:controller PrincipalController
+```
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Iniciando Curso LaraveL";
-});
+Route::get('/', 'PrincipalController@principal');
 
 Route::get('/sobre-nos', function () {
     return "Sobre Nós";
