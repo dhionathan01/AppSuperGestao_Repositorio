c37f32a9791d3b2b4d95b816cf4e09f46cb9f6c2
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-18 08:47:22 -0300

    feat: :sparkles: Criando views e redenizando nos controllers
    Criandos a views contato, sobre-nos, principal e redenizando em seus respectivos controllers

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
-        echo "ContatoController -> contato() executado";
+        return view('site.contato');
     }
 }
--- app_super_gestao/app/Http/Controllers/SobreNosController.php
+++ app_super_gestao/app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function sobreNos(){
-        echo "SobreNosController -> sobreNos() executado";
+        return view('site.sobre-nos');
     }
 }
