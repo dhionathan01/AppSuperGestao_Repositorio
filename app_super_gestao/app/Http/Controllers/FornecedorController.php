bda319ba3400d53945f3333a22f06d8517f397cf
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 17:25:01 -0300

    feat: :sparkles: @Blade switch case
    Utilizando switch case com blade

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -11,11 +11,23 @@ class FornecedorController extends Controller
             0 =>[
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '0'
+                'cnpj' => '0',
+                'ddd' => '11', // São Paulo (SP)
+                'telefone' => '0000-0000'
             ],
             1 =>[
                 'nome' => 'Fornecedor 2',
-                'status' => 'S'
+                'status' => 'N',
+                'cnpj' => null,
+                'ddd' => null, // Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
+            2 =>[
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', // Juiz de Fora (MG)
+                'telefone' => '0000-0000'
             ],
         ];
         $msgFornecedor1 =  isset($fornecedores[0]['cnpj']) ? 'Fornecedor 1: CNPJ informado <br>' : 'Fornecedor 1: CNPJ não informado <BR>';
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -13,23 +13,50 @@
 @endphp
 
 @isset($fornecedores)
-    <!--
-    Operador condicional de valor default (??)
-    $variavel testada não estiver definida (isset)
-        ou
-    $variavel testada possui o valor null
-    -->
-    Fornecedor : {{$fornecedores[0]['nome']}}
-    <br>
-    Status : {{$fornecedores[0]['status']}}
-    <br>
-    CNPJ : {{$fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido'}}
-    <br>
-    <hr>
     Fornecedor : {{$fornecedores[1]['nome']}}
     <br>
     Status : {{$fornecedores[1]['status']}}
     <br>
-    CNPJ : {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}
-
+    CNPJ : {{$fornecedores[1]['cnpj'] ?? ''}}
+    <br>
+    Telefone : {{$fornecedores[1]['ddd'] ?? ''}}  {{$fornecedores[1]['telefone'] ?? ''}}
+    <br>
+    Cidade:
+    @switch($fornecedores[1]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado Não identificado
+    @endswitch
+    <br>
+    <hr>
+    Fornecedor : {{$fornecedores[0]['nome']}}
+    <br>
+    Status : {{$fornecedores[0]['status']}}
+    <br>
+    CNPJ : {{$fornecedores[0]['cnpj'] ?? ''}}
+    <br>
+    Telefone : {{$fornecedores[0]['ddd'] ?? ''}}  {{$fornecedores[1]['telefone'] ?? ''}}
+    <br>
+    Cidade:
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado Não identificado
+    @endswitch
 @endisset
