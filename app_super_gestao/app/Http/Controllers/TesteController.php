150352c7acbf091a77a770b8be3573981e4ef4c2
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 00:28:05 -0300

    feat: :sparkles: Enviado Parametros para view com método compact do PHP
    Enviando parametros do controller para view com o metodo compact, e fazendo a chamada desses valores na view

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -8,6 +8,9 @@ class TesteController extends Controller
 {
     public function teste( int $p1, int $p2){
         /* echo "Soma de {$p1} + {$p2} = ".($p1 + $p2); */
-        return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // Os índices passados se tornam arrays associativos;
+        //return view('site.teste', ['p1' => $p1, 'p2' => $p2]); //array associativo
+
+        // No compact vc passa o nome das variáveis que vc quer e ele as redireciona para view
+        return view('site.teste', compact('p1', 'p2'));// compact
     }
 }
