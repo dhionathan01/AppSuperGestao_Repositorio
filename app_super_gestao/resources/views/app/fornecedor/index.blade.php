be78e41c74a8ecb4cd07e517da8a1faac6558960
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 01:04:39 -0300

    feat: :sparkles: incluido blocos php puro no blade

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,2 +1,10 @@
 <h3>Fornecedor</h3>
 {{-- Comentário que será descartado pelo interpretador do blade --}}
+@php
+    // Para comentar uma linha dentro de um bloco php
+    /* Comentários
+        de
+        Multiplas de comentário
+    */
+    echo "php teste";
+@endphp
