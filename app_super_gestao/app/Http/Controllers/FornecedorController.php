4533eb55f37ac59649d5ece1fbea79df28096e28
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 14:22:29 -0300

    feat: :sparkles: If/Else @blade

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -7,8 +7,7 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = ['Fornecedor 1'];
-
+        $fornecedores = ['Fornecedor 1','Fornecedor 1','Fornecedor 1'];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -7,5 +7,12 @@
     }*/
 @endphp
 {{-- A tag @dd é utilizada para exibir variaveis tipo  array --}}
-@dd($fornecedores)
 
+{{count($fornecedores)}}
+@if(count($fornecedores)> 0 && count($fornecedores) < 10)
+    <h3> Existem alguns fornecedores cadastrados </h3>
+@elseif(count($fornecedores)>10)
+    <h3>Existem muitos fornecedores cadastrados!</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
