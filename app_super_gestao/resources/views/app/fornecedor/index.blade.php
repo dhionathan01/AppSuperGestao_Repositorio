5cad781e85a0f44ea3274b1d89a40448d3b78855
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 15:08:44 -0300

    feat: :sparkles: @Blade isset, utilizando isset do blade para validar a definição de variavel e indice

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,18 +1,26 @@
 <h3>Fornecedor</h3>
 @php
 /*
-    if(!<condição>){} Executa uma ação se ela for true
+    if(isset($variavel)){} Executa uma ação se ela for true / isset verifica se a variavel ou indice array foi definido retornando true
 */
 @endphp
-{{-- @unless  executa se a ação for false --}}
-Fornecedor : {{$fornecedores[0]['nome']}}
-<br>
-Status : {{$fornecedores[0]['status']}}
-<br>
-@if(!($fornecedores[0]['status'] == 'S'))
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') {{-- Se o retorno da condição for false --}}
-    Fornecedor Inativo (Com unless)
-@endunless
+
+@isset($fornecedores)
+    Fornecedor : {{$fornecedores[0]['nome']}}
+    <br>
+    Status : {{$fornecedores[0]['status']}}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ : {{$fornecedores[0]['cnpj']}}
+        <br>
+    @endisset
+    <hr>
+    Fornecedor : {{$fornecedores[1]['nome']}}
+    <br>
+    Status : {{$fornecedores[1]['status']}}
+    <br>
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ : {{$fornecedores[1]['cnpj']}}
+        <br>
+    @endisset
+@endisset
