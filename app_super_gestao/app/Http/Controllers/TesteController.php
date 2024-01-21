67f62a5e92be64173d24d984999805dd768f71ca
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 00:24:09 -0300

    feat: :sparkles: Enviado Parametros para view com arrrays associativos
    Enviando parametros do controller para view com arrays associativos, e fazendo a chamada desses valores na view

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste( int $p1, int $p2){
-        echo "Soma de {$p1} + {$p2} = ".($p1 + $p2);
+        /* echo "Soma de {$p1} + {$p2} = ".($p1 + $p2); */
+        return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // Os índices passados se tornam arrays associativos;
     }
 }
