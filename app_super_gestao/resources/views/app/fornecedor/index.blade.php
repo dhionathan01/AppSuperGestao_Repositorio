2339d4b3990b10bea9af49e8827bc6216b94e576
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 14:21:05 -0300

    feat: :sparkles: Exibindo variaveis tipo array com blade

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,13 +1,11 @@
 <h3>Fornecedor</h3>
-{{-- Tanto a abertura de chaves duplas quanto a tag de impressao do php são iguais observe a abaixo: --}}
-{{'teste impresao  Blade'}}
-<?='teste impressao php'?>
-
 @php
-    // Para comentar uma linha dentro de um bloco php
-    /* Comentários
-        de
-        Multiplas de comentário
-    */
-    echo "php teste";
+    /*if(){
+
+    }elseif(){
+
+    }*/
 @endphp
+{{-- A tag @dd é utilizada para exibir variaveis tipo  array --}}
+@dd($fornecedores)
+
