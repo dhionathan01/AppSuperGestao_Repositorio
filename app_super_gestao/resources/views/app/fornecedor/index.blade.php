7aa3d73727c19a4ffffecccc5ae110f985814341
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-21 21:41:55 -0300

    feat: :sparkles: @Blade while , implementado o while

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -13,17 +13,19 @@
 @endphp
 
 @isset($fornecedores)
-@for ($i = 0; isset($fornecedores[$i]); $i++)
-    Fornecedor : {{$fornecedores[$i]['nome']}}
-    <br>
-    Status : {{$fornecedores[$i]['status']}}
-    <br>
-    CNPJ : {{$fornecedores[$i]['cnpj'] ?? ''}}
-    <br>
-    Telefone : {{$fornecedores[$i]['ddd'] ?? ''}}  {{$fornecedores[1]['telefone'] ?? ''}}
-    <br>
-    <hr>
-@endfor
+    @php $i = 0 @endphp
+    @while (isset($fornecedores[$i]))
+        Fornecedor : {{$fornecedores[$i]['nome']}}
+        <br>
+        Status : {{$fornecedores[$i]['status']}}
+        <br>
+        CNPJ : {{$fornecedores[$i]['cnpj'] ?? ''}}
+        <br>
+        Telefone : {{$fornecedores[$i]['ddd'] ?? ''}}  {{$fornecedores[1]['telefone'] ?? ''}}
+        <br>
+        <hr>
+        @php $i++ @endphp
+    @endwhile
 @endisset
 <br>
 
