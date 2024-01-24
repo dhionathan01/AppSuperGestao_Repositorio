e65c39985bc504878ae5f01750a61900bc593a45
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-01-24 08:08:41 -0300

    feat: :sparkles: Enviado formularios com metodo get

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
+        var_dump($_GET);
         $titulo = 'Contato (Controller Definição)';
         return view('site.contato', compact('titulo'));
     }
