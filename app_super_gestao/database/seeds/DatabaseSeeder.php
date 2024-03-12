940b23356e8abccfce575a4fbf562b419f5f03b2
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-03-12 08:59:49 -0300

    feat: :sparkles: Executando uma única seeder pelo artisan

--- app_super_gestao/database/seeds/DatabaseSeeder.php
+++ app_super_gestao/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(FornecedoreSeeder::class);
+        $this->call(SiteContatoSeeder::class);
 
     }
 }
