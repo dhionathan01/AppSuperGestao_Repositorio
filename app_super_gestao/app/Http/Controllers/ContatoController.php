b4785209ab65bb4df452b7ea252bda3e03906829
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-03-13 07:52:29 -0300

    feat: :sparkles: Enviando Formulário e salvando no banco

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        //dd($request);
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
+
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        print_r($contato->getAttributes());
         $titulo = 'Contato (Controller Definição)';
         return view('site.contato', compact('titulo'));
     }
