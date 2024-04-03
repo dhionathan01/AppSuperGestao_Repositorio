36f7f0d6b68ac9b4615411b72af9a0dad2b66299
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-03 07:39:36 -0300

    feat: :sparkles: Persistindo dados e redirecionando rota
    Fazendo ajustes para  campo motivo_contatos_id

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        $motivo_contatos= MotivoContato::all();
+        $motivo_contatos = MotivoContato::all();
         $contato = new SiteContato();
         //$contato->fill($request->all());
         //$contato->save();
@@ -16,7 +17,7 @@ class ContatoController extends Controller
 
         print_r($contato->getAttributes());*/
         $titulo = 'Contato (Controller Definição)';
-        return view('site.contato', compact('titulo','motivos_contato'));
+        return view('site.contato', compact('titulo','motivo_contatos'));
     }
 
     public function salvar(Request $request){
@@ -25,9 +26,10 @@ class ContatoController extends Controller
             'nome' => 'required|min:3|max:40', // nomes com no mínimo 3 caracteres e no máximo 40
             'telefone' => 'required',
             'email' => 'required|email',
-            'motivo_contato' => 'required',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000',
         ]);
         SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
