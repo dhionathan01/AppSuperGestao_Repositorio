84e87fa954829334e075ede4294bb15df9d7d3bf
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-07 21:26:46 -0300

    feat: :sparkles: Exibição dos erros abaixo do input com ternário

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,10 +2,16 @@
 <form action="{{route('site.contato')}}" method="POST">
     @csrf
     <input name="nome" value="{{old('nome')}}" type="text" placeholder="Nome" class="{{$classe}}">
+    @if($errors->has('nome'))
+        {{$errors->first('nome')}}
+    @endif
     <br>
     <input name="telefone" value="{{old('telefone')}}" type="text" placeholder="Telefone" class="{{$classe}}">
+    {{$errors->has('telefone') ? $errors->first('telefone'): NULL}}
     <br>
     <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
+    {{$errors->has('email') ? $errors->first('email'): NULL}}
+
     <br>
     <select name="motivo_contatos_id" class="{{$classe}}">
         <option value="" >Qual o motivo do contato?</option>
@@ -13,8 +19,12 @@
             <option value="{{$motivo_contato->id}}"{{old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : NULL}}>{{$motivo_contato->motivo_contato}}</option>
         @endforeach
     </select>
+    {{$errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id'): NULL}}
+
     <br>
     <textarea name="mensagem" class="{{$classe}}">{{!empty(old('mensagem')) ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
+    {{$errors->has('mensagem') ? $errors->first('mensagem'): NULL}}
+
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
