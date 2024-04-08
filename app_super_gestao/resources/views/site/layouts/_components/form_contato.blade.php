ec9b454e0fea55f24e33bef48cb5ab61facb4c87
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-07 21:19:44 -0300

    refactor: :recycle: Ajustando exibição e listagem de erros ao submeter formulário

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -18,8 +18,10 @@
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
-<div style="position: absolute; top:0px; left: 0px; width:100%; background:gray;">
-    <pre>
-        {{print_r($errors)}}
-    </pre>
-</div>
+@if($errors->any())
+    <div style="position: absolute; top:0px; left: 0px; width:100%; background:gray;">
+       @foreach (($errors->all()) as $erro)
+            {{$erro}}<BR>
+       @endforeach
+    </div>
+@endif
