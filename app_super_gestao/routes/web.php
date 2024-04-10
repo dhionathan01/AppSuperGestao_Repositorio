65a625e29bba0ce15e649f5a28924339bf1b4075
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-10 08:45:06 -0300

    feat: :sparkles: Passando parametros para o middleware

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::middleware('autenticacao')->prefix('/app')->group(function(){
+Route::middleware('autenticacao:padrao, visitante, p3, p4')->prefix('/app')->group(function(){
     Route::middleware('autenticacao')
         ->get('/clientes', function(){return 'Clientes';})
         ->name('app.clientes');
