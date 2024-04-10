71ae7238990d8ec08049d8cca912b4cc2474a359
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-10 08:31:41 -0300

    feat: :sparkles: Implementando middlewares a grupos de rotas

--- app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
+++ app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
@@ -17,7 +17,7 @@ class AutenticacaoMiddleware
     public function handle($request, Closure $next)
     {
         // verifica se o usuário possui acesso a rota
-        if(true){
+        if(false){
             return $next($request);
         }else{
             return Response('Acesso negado! Rota exige autenticação!!!');
