8da6a5637294f2fbc5a7512469098db29a2a3021
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-10 22:22:22 -0300

    feat: :sparkles: Recebendo parametros usuario e senha

--- app_super_gestao/app/Http/Controllers/LoginController.php
+++ app_super_gestao/app/Http/Controllers/LoginController.php
@@ -9,7 +9,19 @@ class LoginController extends Controller
     public function index(){
         return view('site.login', ['titulo' => 'Login']);
     }
-    public function autenticar(){
-       return 'Chegamos até aqui';
+    public function autenticar(Request $request){
+       // Regras de validação
+       $regras = [
+        'usuario' => 'email',
+        'senha' => 'required'
+       ];
+
+       // Mensagens de feedback de validação
+       $feedback =[
+        'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+        'senha.required' => 'O campo senha é obrigatório'
+       ];
+       $request->validate($regras, $feedback);
+       print_r($request->all());
     }
 }
