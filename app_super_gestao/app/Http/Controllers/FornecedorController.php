154966c25f52f73644f1e547bb4d24857616e2d4
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-22 11:14:32 -0300

    feat: :sparkles: Implementando paginação Laravel

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -15,8 +15,8 @@ class FornecedorController extends Controller
         ->where('site', 'like', '%'.$request->input('site').'%')
         ->where('uf', 'like', '%'.$request->input('uf').'%')
         ->where('email', 'like', '%'.$request->input('email').'%')
-        ->get();
-        return view('app.fornecedor.listar', array('fornecedores'=>$fornecedores));
+        ->paginate(2);
+        return view('app.fornecedor.listar', array('fornecedores'=>$fornecedores, 'request' => $request->all()));
     }
     public function adicionar(Request $request){
         $msg = '';
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('autenticacao:padrao, visitante, p3, p4')->prefix('/app')->gro
         ->name('app.cliente');
 
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::get('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
     Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
     Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
     Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
