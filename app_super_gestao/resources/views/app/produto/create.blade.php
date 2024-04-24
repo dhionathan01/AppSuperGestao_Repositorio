8bb2956e39dc147659f8437c8b8d2817824d10bf
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-24 17:26:06 -0300

    refactor: :recycle: Modificando forma de edição
    Tornando a view create adequada tanto para criação quanto atualização dos registros

--- app_super_gestao/resources/views/app/produto/create.blade.php
+++ app_super_gestao/resources/views/app/produto/create.blade.php
@@ -4,7 +4,11 @@
 @section('conteudo')
     <div class="conteudo-pagina">
         <div class="titulo-pagina-2">
-            <p>Produto - Adicionar</p>
+            @if (isset($produto->id))
+                <p>Produto - Editar</p>
+            @else
+                <p>Produto - Adicionar</p>
+            @endif
         </div>
         <div class="menu">
             <ul>
@@ -14,22 +18,28 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left:auto; margin-right:auto;">
-                <form action="{{route('produto.store')}}" method="post">
+                @if (isset($produto->id))
+                    <form action="{{route('produto.update', ['produto' => $produto->id])}}" method="post">
                     @csrf
-                    <input type="text" name="nome" id="Nome" placeholder="Nome" value="{{old('nome')}}" class="borda-preta">
+                    @method('PUT');
+                @else
+                    <form action="{{route('produto.store')}}" method="post">
+                    @csrf
+                @endif
+                    <input type="text" name="nome" id="Nome" placeholder="Nome" value="{{$produto->nome ?? old('nome')}}" class="borda-preta">
                     {{$errors->has('nome') ? $errors->first('nome') : ''}}
-                    <input type="text" name="descricao" id="Descricao" placeholder="Descrição" value="{{old('descricao')}}" class="borda-preta">
+                    <input type="text" name="descricao" id="Descricao" placeholder="Descrição" value="{{$produto->descricao ?? old('descricao')}}" class="borda-preta">
                     {{$errors->has('descricao') ? $errors->first('descricao') : ''}}
-                    <input type="text" name="peso"   id="Peso" placeholder="Peso" value="{{old('peso')}}" class="borda-preta">
+                    <input type="text" name="peso"   id="Peso" placeholder="Peso" value="{{$produto->peso ?? old('peso')}}" class="borda-preta">
                     {{$errors->has('peso') ? $errors->first('peso') : ''}}
                     <select name="unidade_id" id="Unidade_Id">
                         <option value=""> -- Selecione a unidade de Medida --</option>
                         @foreach ($unidades as $unidade)
-                        <option {{old('unidade_id') == $unidade->id ? 'selected' : NULL}} value="{{$unidade->id}}">{{$unidade->descricao}}</option>
+                        <option {{($produto->unidade_id ?? old('unidade_id')) == $unidade->id ? 'selected' : NULL}} value="{{$unidade->id}}">{{$unidade->descricao}}</option>
                         @endforeach
                     </select>
                     {{$errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}
-                    <button type="submit" class="borda-preta">Cadastrar</button>
+                    <button type="submit" class="borda-preta">{{isset($produto->id) ? 'Atualizar' : 'Cadastrar'}}</button>
                 </form>
             </div>
         </div>
