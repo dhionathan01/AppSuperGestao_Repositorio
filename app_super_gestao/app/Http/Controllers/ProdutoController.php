f7bd7a72bf900e6006aeb8ae8f9a28d246ce13f8
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-24 12:03:00 -0300

    feat: :sparkles: DELETE  implementando Produtos (destroy) -> Deletando produtos

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -103,6 +103,7 @@ class ProdutoController extends Controller
      */
     public function destroy(Produto $produto)
     {
-        //
+        $produto->delete();
+        return redirect()->route('produto.index');
     }
 }
