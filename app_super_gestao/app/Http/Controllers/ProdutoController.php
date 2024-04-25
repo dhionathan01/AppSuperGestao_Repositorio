80ed04074ed3799acff376e7f0a1ec8f5ee4f409
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-04-25 14:38:41 -0300

    feat: :sparkles: Implementando Relacionamento 1 para 1 de forma manual
    Implementando o relacionamento de produtoDetalhes para o produto de forma manual

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Produto;
 use App\Unidade;
 use Illuminate\Http\Request;
+use App\ProdutoDetalhe;
 
 class ProdutoController extends Controller
 {
@@ -16,6 +17,15 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         $produtos = Produto::paginate(10);
+        foreach($produtos as $key => $produto){
+            $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
+            // Já que utilizamos o first, ele recupera diretamente o objeto ProdutoDetalhe e não uma collection
+            if(isset($produtoDetalhe)){
+                $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
+                $produtos[$key]['largura'] = $produtoDetalhe->largura;
+                $produtos[$key]['altura'] = $produtoDetalhe->altura;
+            }
+        }
         return view('app.produto.index', array('produtos'=>$produtos, 'request' => $request->all()));
     }
 
